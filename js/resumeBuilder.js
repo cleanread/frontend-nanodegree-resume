784eab954a56d80aab276234166f4efc3a2d3aaf
cleanread <16105501+cleanread@users.noreply.github.com>
2019-02-10 22:12:25 -0800

    Clean up old comments for new development

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,15 @@
-$("#main").append("Dale Crane")
+var awesomeThoughts = "I am Dale and I'm ok!";
+//var age = 40;
+//var funThoughts = awesomeThoughts.replace("ok", "FUN");
+//console.log(firstName);
+
+var formattedName = HTMLheaderName.replace("%data%", "Dale Crane");
+var formattedRole = HTMLheaderRole.replace("%data%", "Scrum Master");
+
+//var slicedThoughts = awesomeThoughts.slice(-1,0);
+
+var slicedThoughts = awesomeThoughts;
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#main").append(slicedThoughts);
